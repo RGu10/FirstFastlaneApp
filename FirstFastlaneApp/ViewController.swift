3d57edc5c261c54bf11fcfced1bf9c476ae75d95
Ryad Guerroudj <ryad.guerroudj@gmail.com>
2018-07-15 14:05:36 +0200

    Initial Commit

---
+++ FirstFastlaneApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstFastlaneApp
+//
+//  Created by Ryad on 15.07.18.
+//  Copyright © 2018 Biruni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
